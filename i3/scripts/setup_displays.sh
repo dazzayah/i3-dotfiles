96a563f28980af2253f21c4bfc0a01709bf42e9a
dazzayah <dazzayah90032@gmail.com>
2025-03-24 12:49:50 +0100

    config dotfiles added

---
+++ i3/scripts/setup_displays.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Set the resolution and refresh rate for DP-2 and position it to the right of HDMI-0
+xrandr --output DP-1 --mode 1920x1080 --rate 60 --primary
---
+++ polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/docky/config.ini &
+  done
+else
+  polybar -c ~/.config/polybar/docky/config.ini &
+fi
